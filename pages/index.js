bb53a648f1179e99737d9d39a13f205f8e86c736
Aleksei Chyrva <alexey.chyrva@gmail.com>
2020-08-08 00:51:33 +0300

    - create component for a Footer - update styles for sticky Footer - move header to _app

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,7 @@
-import Head from 'next/head'
-
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Covid Patrol</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main >
-        <h1>
-          Welcome to Covid Patrol
-        </h1>
-      </main>
-
-      <footer>
-          Powered by bad-devs
-      </footer>
-    </div>
+    <h1>
+      Welcome to Covid Patrol
+    </h1>
   )
 }
