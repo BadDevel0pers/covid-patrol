d16b5b23b5221d10d0f3432f98654dacf9453386
kholiavko-roman <roman.kholiavko@moovweb.com>
2020-08-10 00:50:12 +0300

    EDIT: pwa configs

--- pages/_document.js
+++ pages/_document.js
@@ -40,13 +40,10 @@ class MyDocument extends Document {
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="default" />
           <meta name="apple-mobile-web-app-title" content="PWA App" />
-          <meta name="description" content="Best PWA App in the world" />
+          <meta name="description" content="Covid Patrol PWA" />
           <meta name="format-detection" content="telephone=no" />
           <meta name="mobile-web-app-capable" content="yes" />
-          <meta name="msapplication-config" content="/static/icons/browserconfig.xml" />
-          <meta name="msapplication-TileColor" content="#2B5797" />
-          <meta name="msapplication-tap-highlight" content="no" />
-          <meta name="theme-color" content="#000000" />
+          <meta name="theme-color" content="#3f51b5" />
           <meta
             name="viewport"
             content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
@@ -58,20 +55,6 @@ class MyDocument extends Document {
           <link rel="manifest" href="/static/manifest.json" />
           <link rel="mask-icon" href="/static/icons/safari-pinned-tab.svg" color="#5bbad5" />
           <link rel="shortcut icon" href="/static/icons/favicon.ico" />
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
-
-          <meta name="twitter:card" content="summary" />
-          <meta name="twitter:url" content="https://yourdomain.com" />
-          <meta name="twitter:title" content="PWA App" />
-          <meta name="twitter:description" content="Best PWA App in the world" />
-          <meta name="twitter:image" content="https://yourdomain.com/static/icons/android-chrome-192x192.png" />
-          <meta name="twitter:creator" content="@DavidWShadow" />
-          <meta property="og:type" content="website" />
-          <meta property="og:title" content="PWA App" />
-          <meta property="og:description" content="Best PWA App in the world" />
-          <meta property="og:site_name" content="PWA App" />
-          <meta property="og:url" content="https://yourdomain.com" />
-          <meta property="og:image" content="https://yourdomain.com/static/icons/apple-touch-icon.png" />
         </Head>
         <body>
           <Main />
