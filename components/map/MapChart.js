225f7cc8d42ef116160f0b8f566fed5a21980a9b
kholiavko-roman <roman.kholiavko@moovweb.com>
2020-08-09 12:51:02 +0300

    ADD: change zoom and position by clicking on map

--- components/map/MapChart.js
+++ components/map/MapChart.js
@@ -1,5 +1,5 @@
 import React, { memo, useState } from 'react'
-import { ComposableMap, Geographies, Geography, Graticule } from 'react-simple-maps'
+import { ComposableMap, Geographies, Geography, Graticule, ZoomableGroup } from 'react-simple-maps'
 import { makeStyles, useTheme } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
 import WorldIcon from '@material-ui/icons/Public'
@@ -11,9 +11,12 @@ import MapPopover from './MapPopover'
 import MapSideBar from './MapSideBar'
 import mapData from '../../helpers/map/mapData'
 
+const MAP_HEIGHT = 500
+
 const styles = theme => ({
   root: {
     position: 'relative',
+    maxHeight: '80vh',
   },
   buttonContainer: {
     '& button + button': {
@@ -29,22 +32,40 @@ const MapChart = ({ setTooltipContent, setTooltipAnchor }) => {
   const theme = useTheme()
 
   const [geographies] = useState(feature(mapData, mapData.objects[Object.keys(mapData.objects)[0]]).features)
-  const [position, setPosition] = useState({ coordinates: [0, 0], zoom: 120 })
+  const [position, setPosition] = useState({ coordinates: [0, 0], zoom: 1 })
   const [anchorEl, setAnchorEl] = useState(null)
   const [popoverContent, setPopoverContent] = useState(null)
   const [isWorldMapType, setIsWorldMapType] = useState(true)
   const [selectedCountry, setSelectedCountry] = useState(null)
 
   const handleZoomIn = () => {
-    if (position.zoom >= 600) return
+    if (position.zoom >= 8) return
 
     setPosition(pos => ({ ...pos, zoom: pos.zoom * 1.5 }))
+
+    if (anchorEl) {
+      setAnchorEl(null)
+      setSelectedCountry(null)
+    }
   }
 
   const handleZoomOut = () => {
-    if (position.zoom <= 100) return
+    if (position.zoom <= 1) return
 
     setPosition(pos => ({ ...pos, zoom: pos.zoom / 1.5 }))
+
+    if (anchorEl) {
+      setAnchorEl(null)
+      setSelectedCountry(null)
+    }
+  }
+
+  const handleMapMove = mapDetails => {
+    console.log('mapDetails ', mapDetails)
+    setPosition({
+      coordinates: mapDetails.coordinates,
+      zoom: mapDetails.zoom,
+    })
   }
 
   const handleClick = (event, geo) => {
@@ -52,7 +73,7 @@ const MapChart = ({ setTooltipContent, setTooltipAnchor }) => {
       properties: { ISO_A2: countryCode, NAME: countryName },
     } = geo
 
-    setPosition({ coordinates: geoCentroid(geo), zoom: 400 })
+    setPosition({ coordinates: geoCentroid(geo), zoom: 4 })
     setAnchorEl(event.currentTarget)
     setPopoverContent(`${countryCode} ${countryName}`)
     setSelectedCountry(countryCode)
@@ -70,6 +91,8 @@ const MapChart = ({ setTooltipContent, setTooltipAnchor }) => {
   const handleUSAMapClick = () => {
     setIsWorldMapType(false)
   }
+
+  console.log('position ', position.coordinates)
 
   return (
     <div className={classes.root}>
@@ -95,82 +118,97 @@ const MapChart = ({ setTooltipContent, setTooltipAnchor }) => {
       </div>
 
       <ComposableMap
-        projectionConfig={{
-          center: position.coordinates,
-          scale: position.zoom,
-        }}
+        // projectionConfig={{
+        //   center: position.coordinates,
+        //   scale: position.zoom,
+        // }}
+        height={MAP_HEIGHT}
       >
-        <Graticule stroke={theme.palette.map.border} strokeWidth={0.5} onClick={handleClosePopover} />
-        <Geographies geography={geographies}>
-          {({ geographies }) =>
-            geographies.map((geo, index) => {
-              const geographyStyles = {
-                default: {
-                  fill: theme.palette.map.default,
-                  outline: 'none',
-                  stroke: theme.palette.map.border,
-                },
-                hover: {
-                  fill: theme.palette.warning.light,
-                  outline: 'none',
-                },
-                pressed: {
-                  fill: theme.palette.warning.light,
-                  outline: 'none',
-                },
-              }
-              const {
-                properties: { ISO_A2: countryCode, NAME: countryName },
-              } = geo
+        <ZoomableGroup
+          zoom={position.zoom}
+          center={position.coordinates}
+          onMoveStart={pos => {
+            return false
+          }}
+          onMove={pos => {
+            return false
+          }}
+          onMoveEnd={handleMapMove}
+        >
+          <Graticule stroke={theme.palette.map.border} strokeWidth={0.5} onClick={handleClosePopover} />
+          <Geographies geography={geographies}>
+            {({ geographies }) =>
+              geographies.map((geo, index) => {
+                const geographyStyles = {
+                  default: {
+                    fill: theme.palette.map.default,
+                    outline: 'none',
+                    stroke: theme.palette.map.border,
+                    strokeWidth: '0.35',
+                  },
+                  hover: {
+                    fill: theme.palette.warning.light,
+                    outline: 'none',
+                  },
+                  pressed: {
+                    fill: theme.palette.warning.light,
+                    outline: 'none',
+                  },
+                }
+                const {
+                  properties: { ISO_A2: countryCode, NAME: countryName },
+                } = geo
 
-              // For demo
-              if (!isWorldMapType) {
-                geographyStyles.default.fill = theme.palette.error.main
-                geographyStyles.hover.fill = theme.palette.error.dark
-                geographyStyles.hover.pressed = theme.palette.error.dark
+                // For demo
+                if (!isWorldMapType) {
+                  geographyStyles.default.fill = theme.palette.error.main
+                  geographyStyles.hover.fill = theme.palette.error.dark
+                  geographyStyles.hover.pressed = theme.palette.error.dark
 
-                if (index % 2 === 1) {
-                  geographyStyles.default.fill = theme.palette.warning.main
-                  geographyStyles.hover.fill = theme.palette.warning.dark
-                  geographyStyles.hover.pressed = theme.palette.warning.dark
+                  if (index % 2 === 1) {
+                    geographyStyles.default.fill = theme.palette.warning.main
+                    geographyStyles.hover.fill = theme.palette.warning.dark
+                    geographyStyles.hover.pressed = theme.palette.warning.dark
+                  }
+
+                  if (index % 5 === 1) {
+                    geographyStyles.default.fill = theme.palette.success.main
+                    geographyStyles.hover.fill = theme.palette.success.dark
+                    geographyStyles.hover.pressed = theme.palette.success.dark
+                  }
                 }
 
-                if (index % 5 === 1) {
-                  geographyStyles.default.fill = theme.palette.success.main
-                  geographyStyles.hover.fill = theme.palette.success.dark
-                  geographyStyles.hover.pressed = theme.palette.success.dark
+                if (selectedCountry === countryCode) {
+                  geographyStyles.default.fill = geographyStyles.hover.fill
                 }
-              }
 
-              if (selectedCountry === countryCode) {
-                geographyStyles.default.fill = geographyStyles.hover.fill
-              }
+                if (selectedCountry) {
+                  geographyStyles.hover.fill = theme.palette.map.default
+                }
 
-              if (selectedCountry) {
-                geographyStyles.hover.fill = theme.palette.map.default
-              }
-
-              return (
-                <Geography
-                  id={countryCode}
-                  key={geo.rsmKey}
-                  geography={geo}
-                  onClick={event => handleClick(event, geo)}
-                  onMouseEnter={event => {
-                    setTooltipContent(countryName)
-                    setTooltipAnchor(event.currentTarget)
-                  }}
-                  onMouseLeave={() => {
-                    setTooltipContent('')
-                    setTooltipAnchor(null)
-                  }}
-                  style={geographyStyles}
-                />
-              )
-            })
-          }
-        </Geographies>
+                return (
+                  <Geography
+                    id={countryCode}
+                    key={geo.rsmKey}
+                    geography={geo}
+                    onClick={event => handleClick(event, geo)}
+                    onMouseEnter={event => {
+                      setTooltipContent(countryName)
+                      setTooltipAnchor(event.currentTarget)
+                    }}
+                    onMouseLeave={() => {
+                      setTooltipContent('')
+                      setTooltipAnchor(null)
+                    }}
+                    style={geographyStyles}
+                  />
+                )
+              })
+            }
+          </Geographies>
+        </ZoomableGroup>
       </ComposableMap>
+
       <MapPopover anchorEl={anchorEl} onClose={handleClosePopover} content={popoverContent} />
 
       <MapSideBar
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,10 @@ export default function Home() {
   return (
     <>
       <h1>Welcome to Covid Patrol</h1>
+      {/*// TODO: remove, this is temporary*/}
+      <div style={{ height: '50vh' }} />
       <Map />
+      <div style={{ height: '300vh' }} />
     </>
   )
 }
